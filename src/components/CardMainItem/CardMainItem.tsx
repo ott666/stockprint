a939ac639987a1d4dcc004fe46e040bbf0261b6b
ott <ott@debian>
2023-08-02 16:12:07 -0300

    atualizacao da tarde

--- src/components/CardMainItem/CardMainItem.tsx
+++ src/components/CardMainItem/CardMainItem.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 
 interface CardMainItemProps{
-    url: string
+    url: any
     url2:any
     alt: string
     text: string
@@ -12,13 +12,12 @@ interface CardMainItemProps{
 export function CardMainItem(props: CardMainItemProps){
     
     return(
-        <div>
-            <div className="flex flex-wrap w-[100%]">
-                <div className={`flex flex-col h-[450px] w-[275px] bg-${props.url}`}>
+        
+                <div className={`flex flex-col h-[450px] w-[275px] relative`}>
+                    <Image src={props.url} fill={true} alt='bg' className="absolute z-[-1]"/>
                     <h2 className="text-4xl font-sans text-center text-white font-bold">{props.text}</h2>
-                    <Image src={props.url2} height={450} width={275} alt={props.alt}/>
-                </div>
+                    <Image src={props.url2} height={450} width={275} alt={props.alt} className="absolute"/>
+
             </div>
-        </div>
     )
 }
--- src/components/OurProducts/OurProducts.tsx
+++ src/components/OurProducts/OurProducts.tsx
@@ -5,16 +5,23 @@ import ImgCard1 from '@/assets/img/ink1000.png'
 import ImgCard2 from '@/assets/img/ribbon.png'
 import ImgCard3 from '@/assets/img/kit.png'
 import ImgCard4 from '@/assets/img/imp_etqt.png'
-
+import BgCard1 from '@/assets/img/fundo1.png'
+import BgCard2 from '@/assets/img/fundo2.png'
+import BgCard3 from '@/assets/img/fundo3.png'
+import BgCard4 from '@/assets/img/fundo4.png'
 
 export function OurProducts(){
     return(
         <Container>
-            <div className="flex flex-wrap justify-between w-[100%] pt-[50px]">
-            <CardMainItem url='img_bg_card1' text='Impressoras de cartao' url2={ImgCard1} alt='HID FARGO INK1000'/>
-            <CardMainItem url='img_bg_card2' text='Impressoras de cartao' url2={ImgCard2} alt='Ribbons 084051'/>
-            <CardMainItem url='img_bg_card3' text='Impressoras de cartao' url2={ImgCard3} alt='Kit 086177'/>
-            <CardMainItem url='img_bg_card4' text='Impressoras de cartao' url2={ImgCard4} alt='Zebra ZD220'/>
+            <div className="flex flex-col w-[100%] h-[100%]">
+                <h1 className="text-center  text-4xl">Nossos produtos</h1>
+                <div className="flex flex-wrap justify-between w-[100%] pt-[50px]">
+                    <CardMainItem url={BgCard1} text='Impressoras de cartao' url2={ImgCard1} alt='HID FARGO INK1000'/>
+                    <CardMainItem url={BgCard2} text='Impressoras de cartao' url2={ImgCard2} alt='Ribbons 084051'/>
+                    <CardMainItem url={BgCard3} text='Impressoras de cartao' url2={ImgCard3} alt='Kit 086177'/>
+                    <CardMainItem url={BgCard4} text='Impressoras de cartao' url2={ImgCard4} alt='Zebra ZD220'/>
+                </div>
+            
             </div>
      
         </Container>
