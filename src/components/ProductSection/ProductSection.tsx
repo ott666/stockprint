a92f100bf5711e94ae58a981ea25d23de319fd2e
ott666 <ottaviocavarzani@gmail.com>
2023-08-06 15:56:51 -0300

    att

--- src/components/ProductSection/ProductSection.tsx
+++ src/components/ProductSection/ProductSection.tsx
@@ -20,7 +20,7 @@ const ProductSection =(props:any) =>{
            <Carousel>
            {fotosProduto.map((produto)=>(
             <div key={props.lista.pseudo} className="bg-cover">
-                <img  src={produto} alt={produto}/>
+                <img src={`${produto}`} alt={`${produto}`}/>
             </div>
            ))}
            </Carousel>
