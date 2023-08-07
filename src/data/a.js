a53bdcbf4a113be4bd2778636f9c66c7cd20a4f3
ott666 <ottaviocavarzani@gmail.com>
2023-08-07 11:10:33 -0300

    'shadddd'

---
+++ src/data/a.js
@@ -0,0 +1,237 @@
+
+/* 
+
+045432
+[
+    "Impressoras compatíveis: Fargo C-50",
+    "Fabricante: HID FARGO",
+    "Rendimento: 250 faces coloridas",
+    "Painéis: Amarelo (Y), Magenta(M), Ciano(C), Preto(K), Overlay(O)"
+]
+
+045102
+[
+    "Impressoras compatíveis: DTC1000 e DTC1250e (single e dual)",
+    "Fabricante: HID FARGO",
+    "Rendimento: 1000 faces monocromáticas em preto",
+    "Painéis: Resina (K)"
+]
+
+
+
+072214 
+[
+    "Impressoras compatíveis: HDP5000 (MODELO GUARDIAN)",
+    "Fabricante: HID FARGO",
+    "Rendimento: 500 faces coloridas",
+    "Painéis:  Amarelo (Y), Magenta(M), Ciano(C), Preto(K)",
+    "Atenção: este Ribbon deve ser usado juntamente com o Filme #084053",
+    "Atenção (novamente): este Ribbon é compatível com o modelo GUARDIAN da HDP5000"
+]
+
+
+
+
+
+
+045010
+[
+    "Impressoras compatíveis:DTC1000 e DTC1250e (single e dual)",
+    "Fabricante: HID FARGO",
+    "Rendimento: 200 faces coloridas",
+    "Painéis: Amarelo (Y), Magenta(M), Ciano(C), Preto(K), Overlay(O), Preto(K)",
+    "Aplicação: ideal para quem imprime frente colorida e verso preto.",
+],
+
+
+
+045106
+[
+    "Impressoras compatíveis:DTC1000 e DTC1250e (single e dual)",
+    "Fabricante: HID FARGO",
+    "Rendimento: 1000 faces monocromáticas em branco",
+    "Painéis: Branco (W)",
+],
+
+
+
+
+
+[
+    "Filme Retransfer #072213",
+    "Impressoras compatíveis: HDP5000 (MODELO GUARDIAN)",
+    "Fabricante: HID FARGO",
+    "Rendimento: 750 faces (independente da cor)",
+    "Atenção: este Filme deve ser usado juntamente com o Ribbon #084051",
+    "Atenção (novamente): este Filme é compatível com o modelo GUARDIAN da HDP5000",
+],
+
+
+
+
+Ribbon #045610 - YMCKO
+[
+    "Impressoras compatíveis: DTC1500 (single e dual)",
+    "Fabricante: HID FARGO",
+    "Rendimento: 500 faces coloridas",
+    "Painéis: Amarelo (Y), Magenta(M), Ciano(C), Preto(K), Overlay(O)",
+],
+
+
+
+
+
+[
+    "Polyguard #082601",
+    "Impressoras compatíveis:DTC1500 e HDP5000 (single e dual)",
+    "Fabricante: HID FARGO",
+    "Rendimento: 250 faces independente da cor",
+    "Painéis: não possui recorte para chip de contato",
+],
+
+
+
+
+
+
+
+
+
+[
+"Filme Retransfer #084900",
+"Impressoras compatíveis: HDP6600",
+"Fabricante: HID FARGO",
+"Rendimento: 1500 faces (independente da cor)",
+"Atenção: este filme deve ser usado juntamente com o Ribbon #084911",
+],
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+[
+    "Ribbon #084911 - YMCK",
+    "Impressoras compatíveis: HDP6600",
+    "Fabricante: HID FARGO",
+    "Rendimento: 750 faces coloridas",
+    "Painéis: Amarelo (Y), Magenta(M), Ciano(C), Preto(K)",
+    "Atenção: este Ribbon deve ser usado juntamente com o Filme #084900",
+],
+
+
+
+
+
+
+
+
+
+
+
+
+[
+"    Ribbon Colorido #084051 - YMCK",
+"Impressoras compatíveis: HDP5000",
+"Fabricante: HID FARGO",
+"Rendimento: 500 faces coloridas",
+"Painéis: Amarelo (Y), Magenta(M), Ciano(C), Preto(K)",
+"Atenção: este Ribbon deve ser usado juntamente com o Filme #084053",
+],
+
+
+
+
+
+
+
+
+
+
+
+
+
+[
+"    Cartucho de Tinta - YMC",
+"Impressoras compatíveis: HID FARGO INK1000",
+"Fabricante: HID FARGO",
+"Rendimento: até 1300 faces coloridas",
+"Tinta: Amarelo (Y), Magenta(M), Ciano(C). Preto COMPOSTO",
+],
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+[
+"Polyguard #072216",
+"Impressoras compatíveis: DTC1500 e HDP5000 (single e dual MODELO GUARDIAN)",
+"Fabricante: HID FARGO",
+"Rendimento: 250 faces independente da cor",
+"Painéis: não possui recorte para chip de contato",
+],
+
+
+
+
+
+
+
+
+
+
+
+[
+"Filme Retransfer #084053",
+"Impressoras compatíveis: HDP5000",
+"Fabricante: HID FARGO",
+"Rendimento: 750 faces (independente da cor)",
+"Atenção: este Filme deve ser usado juntamente com o Ribbon #084051",
+],
+
+
+
+
+
+
+
+
+
+
+
+
+[
+"    Ribbon #045000 - YMCKO",
+"Impressoras compatíveis: DTC1000 e DTC1250e (single e dual)",
+"Fabricante: HID FARGO",
+"Rendimento: 250 faces coloridas",
+"Painéis: Amarelo (Y), Magenta(M), Ciano(C), Preto(K), Overlay(O)",
+],
+
+
+
+
+
+*/
+
+
+
+
